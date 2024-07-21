94ad08fb4091b6ef7fea0413fa80645f4b684b47
Toyosi Olaribigbe <ot.olaribigbe@outlook.com>
2024-07-21 23:25:32 +0100

    refactor database

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,7 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
+    connectionString: "postgresql://mypostgres_5gtw_user:VYokwcuiBimzS1IrxsO1L8mOSzdr9Z40@dpg-cqdgtchu0jms738pagv0-a.frankfurt-postgres.render.com/mypostgres_5gtw",
     user:"mypostgres_5gtw_user",
     password: "VYokwcuiBimzS1IrxsO1L8mOSzdr9Z40",
     host: "dpg-cqdgtchu0jms738pagv0-a.frankfurt-postgres.render.com",
