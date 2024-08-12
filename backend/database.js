e1b2824367637bd68cb863097c687a489d53652f
Toyosi Olaribigbe <ot.olaribigbe@outlook.com>
2024-08-12 14:43:44 +0100

    push

--- backend/database.js
+++ backend/database.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
    
-    connectionString:'postgresql://postgres.cufotyfczmpyugiuetvw:Kangaroo18?@aws-0-eu-west-2.pooler.supabase.com:6543/postgres'
+    connectionString:'postgresql://postgres.cufotyfczmpyugiuetvw:lnAWdfOMpJtQ3Tm8@aws-0-eu-west-2.pooler.supabase.com:6543/postgres'
    
 });
 
