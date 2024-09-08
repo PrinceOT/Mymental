fe20b1e26e24c28be51d75f03a186ebd695d8715
David (Toyosi) Olaribigbe <101530167+PrinceOT@users.noreply.github.com>
2024-09-08 23:01:38 +0100

    Update protectedRoutes.js

--- frontend/src/Auth/protectedRoutes.js
+++ frontend/src/Auth/protectedRoutes.js
@@ -1,39 +1,74 @@
-import {  useNavigate , Outlet } from 'react-router-dom'
+import {  useNavigate , Outlet,Navigate } from 'react-router-dom'
 import { useAuth } from '../Auth/AuthProvider'
 import { useState , useEffect} from 'react';
 import Login from '../Components/SetupUser/login';
 import Navbar from '../Components/Misc/navbar';
+import "./js.css"
 
-const ProtectedRoutes = ({user}) => {
+const ProtectedRoutes = () => {
   const navigate = useNavigate();
-  const [trigger, settrigger] = useState(false);
+  const {user} = useAuth();
+  const [showComponent, setShowComponent] = useState(false); // Initially hidden
+  
+
   useEffect(() => {
+    // Set a timer to update state after a delay
+    const timer = setTimeout(() => {
+     
+      setShowComponent(true); // Show component after delay
+    }, 400); // 3 seconds delay
 
-    const timeoutId = setTimeout(() => {
-      if(!user){
-      settrigger(true)
-      }
-    }, 400);
+    // Clean up the timer if the component unmounts
+    return () => clearTimeout(timer);
+  }, []); // Empty dependency array to run only on mount
 
-    // Cleanup function to clear the timeout if the component unmounts
-    return () => clearTimeout(timeoutId);
-    
-
-    
-  }, [user]);
-
-  const redirect = () => {
-    navigate('/login')
-
-  }
-    
-    
-return(
+  return (
   <>
-  {user && <Outlet/>}
-  {trigger && redirect()}
-  </>
-)
+   {showComponent ? (
+        <>
+       
+         {
+          user ? <Outlet/> : <Navigate to='/login'/>
+         }
+         
+        </>
+      ) : (
+        <div class="loader-container">
+  <div class="loader"></div>
+</div>
+      )}
+   {/* { showComponent ?  <> <Navbar/> {true ? <Outlet/> : <Navigate to='/login'/>)}:(<div>Loading</div>)} */}
+   
+   </>)
+//   const [trigger, settrigger] = useState(false);
+//   useEffect(() => {
+
+//     const timeoutId = setTimeout(() => {
+//       if(!user){
+//       settrigger(true)
+//       }
+//     }, 400);
+
+//     // Cleanup function to clear the timeout if the component unmounts
+//     return () => clearTimeout(timeoutId);
+    
+
+    
+//   }, [user]);
+
+//   const redirect = () => {
+//     navigate('/login')
+
+//   }
+    
+    
+// return(
+//   <>
+//  <Navbar/>
+//   {user && <Outlet/>}
+//   {trigger && redirect()}
+//   </>
+// )
 
     // if(user){
     //   // console.log(user)
